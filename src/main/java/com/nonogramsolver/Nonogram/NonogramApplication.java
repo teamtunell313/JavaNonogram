86ccfbc4a1840a1277951e6818a7439c6e05bb18
Sean Tunell <c-sean.tunell@charter.com>
2023-05-02 16:40:40 -0600

    used Spring Initializr

---
+++ src/main/java/com/nonogramsolver/Nonogram/NonogramApplication.java
@@ -0,0 +1,13 @@
+package com.nonogramsolver.Nonogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nonogramsolver/Nonogram/NonogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonogramsolver.Nonogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
